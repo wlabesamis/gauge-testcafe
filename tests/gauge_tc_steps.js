6027d4833a6a3c75935227cd6556ecf4d86f9aa2
Wilson Abesamis <wabesamis@usautoparts.com>
2020-04-24 16:08:07 +0800

    bring back missing steps
    Signed-off-by: Wilson Abesamis <wabesamis@usautoparts.com>

--- tests/gauge_tc_steps.js
+++ tests/gauge_tc_steps.js
@@ -5,6 +5,21 @@ const tc = require(process.cwd()+'/tests/testcafe/tc');
 const SITE_DOMAIN = process.env.SITE_DOMAIN;
 const TC_ELEM_TIMEOUT = process.env.TESTCAFE_ELEMENT_TIMEOUT;
 
+step("Goto Google's search page", async function () {
+  await tc().navigateTo('http://google.com');
+});
+
+step("Search for <query>", async function (query) {
+  var input = Selector('input[title="Search"]').with({ boundTestRun: _ });
+  await tc().typeText(input, query);
+  await tc().pressKey("enter");
+});
+
+step("First link is <text>", async function(text) {
+  var firstLink = Selector('#rso').find('a').with({ boundTestRun: _ });
+  await tc().expect(firstLink.innerText).contains(text);
+});
+
 step("Step Passed", async() => {
   await assert.equal(1, 1);
 })
